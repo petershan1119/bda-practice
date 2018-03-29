86c2f57cfff2d4cbe9eca61b047756a8d3e64212
Sang Won Han <petershan0315@gmail.com>
2018-03-29 13:01:16 +0900

    another revision

--- schools.R
+++ schools.R
@@ -1,4 +1,5 @@
 #setwd('/Users/sangwonhan/statistics/bda')
+library(rstan)
 schools <- read.csv('schools.csv', header=TRUE)
 J <- nrow(schools)
 y <- schools$estimate
@@ -10,4 +11,12 @@ plot(schools_fit)
 
 print(schools_fit, 'theta[1]', probs=c(0.025, 0.975))
 
+schools_fit1 <- stan(fit=schools_fit, data=c('J', 'y', 'sigma'), iter=2000, chains=4)
 schools_sim <- extract(schools_fit1)
+
+hist(schools_sim$tau)
+
+mean(schools_sim$theta[,1] > schools_sim$thata[,3])
+
+n_sims <- length(schools_sim$lp__)
+y_rep <- array(NA, 
